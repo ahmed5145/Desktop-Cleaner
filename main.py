7b62d9660271f9f334081a136220acbfe6baccbd
ahmed5145 <ahmedmohamed200354@gmail.com>
2024-06-17 22:27:54 -0500

    Final Version Until any Significant Changes

--- main.py
+++ main.py
@@ -1,37 +1,99 @@
+from os import scandir, rename
+from os.path import splitext, exists, join
+from shutil import move
+from time import sleep
+
+import logging
+
 from watchdog.observers import Observer
-import time
 from watchdog.events import FileSystemEventHandler
-import os
-import json
-import shutil
+
+#Directories and Paths Setup
+source_dir = "C:\\Users\\hussah01\\Downloads"
+dest_dir_sfx = "C:\\Users\\hussah01\\Desktop\\Sound"
+dest_dir_music = "C:\\Users\\hussah01\\Desktop\\Music"
+dest_dir_video = "C:\\Users\\hussah01\\Desktop\\Downloaded Videos"
+dest_dir_image = "C:\\Users\\hussah01\\Desktop\\Downloaded Images"
+dest_dir_documents = "C:\\Users\\hussah01\\Desktop\\Downloaded Documents"
+
+image_extensions = [".jpg", ".jpeg", ".jpe", ".jif", ".jfif", ".jfi", ".png", ".gif", ".webp", ".tiff", ".tif", ".psd", ".raw", ".arw", ".cr2", ".nrw",
+                    ".k25", ".bmp", ".dib", ".heif", ".heic", ".ind", ".indd", ".indt", ".jp2", ".j2k", ".jpf", ".jpf", ".jpx", ".jpm", ".mj2", ".svg", ".svgz", ".ai", ".eps", ".ico"]
+
+video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
+                    ".mp4", ".mp4v", ".m4v", ".avi", ".wmv", ".mov", ".qt", ".flv", ".swf", ".avchd"]
+
+audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
+
+document_extensions = [".doc", ".docx", ".odt",
+                       ".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+
+def makeUnique(dest, name):
+    filename, extension = splitext(name)
+    counter = 1
+    while exists(f"{dest}/{name}"):
+        name = f"{filename}({str(counter)}){extension}"
+        counter += 1
+    return name 
+
+
+def move_file(dest, entry, name): 
+    if exists(f"{dest}/{name}"):
+        unique_name = makeUnique(dest, name)
+        old_name = join(dest,name)
+        new_name = join(dest, unique_name)
+        rename(old_name, new_name)
+    move(entry,dest)
 
 # file handler
-class Myhandler (FileSystemEventHandler):
+class MyHandler (FileSystemEventHandler):
     def on_modified(self, event):
-        i =1
-        if filename != 'kalle':
-            new_name = filename
-            split_name = filename.split('.')
-            file_exists = os.path.isfile(folder_destination + "/" + new_name)
-            while file_exists:
-                i+=1
-                new_name = os.path.splitext(folder_to_track + '/' +new_name)[0] + str(i) + os.path.sp
-                new_name = new_name.split("/")[4]
-                file_exists = os.path.isfile(folder_destination +"/"+ new_name)
+        with scandir(source_dir) as entries:
+            for entry in entries:
+                name = entry.name
+                self.check_audio_files(entry, name)
+                self.check_video_files(entry, name)
+                self.check_image_files(entry, name)
+                self.check_document_files(entry, name)
+            def check_audio_files(self, entry, name):
+                for audio_extension in audio_extensions:
+                    if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
+                        if entry.stat().st_size < 10_000_000 or "SFX" in name:
+                            dest = dest_dir_sfx
+                        else:
+                            dest = dest_dir_music
+                        move_file(dest, entry, name)
+                        logging.info(f"Moved audio file: {name}")
                 
-            src = folder_to_track + "/" + filename
-            new_name = folder_destination + "/" +new_name
-            os.rename(src, new_name)
-folder_to_track = '/Users/hussah01/Desktop'
-folder_destination = '/Users/hussah01/Desktop/kalle'
-event_handler = Myhandler()
-observer = Observer()
-observer.scheduele(event_handler,folder_to_track, recursive = True)
-observer.start
+            def check_video_files(self, entry, name):  # * Checks all Video Files
+                for video_extension in video_extensions:
+                    if name.endswith(video_extension) or name.endswith(video_extension.upper()):
+                        move_file(dest_dir_video, entry, name)
+                        logging.info(f"Moved video file: {name}")
+                
+            def check_image_files(self, entry, name):  # * Checks all Image Files
+                for image_extension in image_extensions:
+                    if name.endswith(image_extension) or name.endswith(image_extension.upper()):
+                        move_file(dest_dir_image, entry, name)
+                        logging.info(f"Moved image file: {name}")
 
-try:
-    while True:
-        time.sleep(10)
-except KeyboardInterrupt:
-    observer.stop()
-observer.join() 
+            def check_document_files(self, entry, name):  # * Checks all Document Files
+                for documents_extension in document_extensions:
+                    if name.endswith(documents_extension) or name.endswith(documents_extension.upper()):
+                        move_file(dest_dir_documents, entry, name)
+                        logging.info(f"Moved document file: {name}")
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = source_dir
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
